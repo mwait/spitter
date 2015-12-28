49c29c09c3525008a0fb81592e3f82cb48064604
Mark Wait <m.wait@gmail.com>
2015-12-29 00:06:26 +0100

    Cargo i exec-maven-plugin ValidationMessages wyświetlanie błędów z formularza

--- src/main/java/spittr/config/WebConfig.java
+++ src/main/java/spittr/config/WebConfig.java
@@ -1,8 +1,10 @@
 package spittr.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,7 +13,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({"spittr.web", "spittr.data"})
+@ComponentScan({ "spittr.web", "spittr.data" })
 public class WebConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public ViewResolver viewResolver() {
@@ -19,11 +21,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		resolver.setExposeContextBeansAsAttributes(true);
+		// poniższe ustawienie powoduje produkcję widoków JstlView -
+		// przekazywanie locale i źródła komunikatów skonf. w Springu
+		resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
 		return resolver;
 	}
 
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s" %>
 <%@ page session="false" %>
 <html>
 <head>
@@ -8,8 +9,19 @@ type="text/css"
 href="<c:url value="/resources/style.css" />" >
 </head>
 <body>
-<h1>Welcome to Spittr</h1>
-<a href="<c:url value="/spittles" />">Spittles</a> |
-<a href="<c:url value="/spitter/register" />">Register</a>
+<h1><s:message code="spittr.welcome" /></h1>
+<%-- <a href="<c:url value="/spittles" />">Spittles</a> | --%>
+
+<a href="<s:url value="/spittles" htmlEscape="true">
+<s:param name="max" value="60" />
+<s:param name="count" value="20" />
+</s:url>">Spittles</a>
+
+<a href="<s:url value="/spitter/register" />">Register</a>
+
+<%--Budowanie adresu dla konkretnej strony profilu użytkownika (parametr wstawiany w miejsce sym. zastepczego)
+ <s:url href="/spitter/{username}" var="spitterUrl">
+<s:param name="username" value="jbauer" />
+</s:url> --%>
 </body>
 </html>
