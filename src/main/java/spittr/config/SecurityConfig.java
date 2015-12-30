9db9e2681b222ac1fc83feb8a3ead825945d891f
Mark Wait <m.wait@gmail.com>
2015-12-30 21:39:34 +0100

    Spring Security

---
+++ src/main/java/spittr/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().and()
+		.httpBasic();
+		}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	}
+
+}
